db97196f1643ac9eab85ff47356fd133ee606546
Radheranij <157111271+Radheranij@users.noreply.github.com>
2024-01-24 23:11:01 +0530

    Update bot.py

--- AlexaMusic/core/bot.py
+++ AlexaMusic/core/bot.py
@@ -1,9 +1,9 @@
-# Copyright (C) 2024 by Krishna_Help @ Github, < https://github.com/official_radhe_krishna_1>
+# Copyright (C) 2024 by Krishna_Help @ Github, < https://github.com/Radheranij/Poison_x_music >
 # Subscribe On YT < official_radhe_krishna_1 >. All rights reserved. © Poison © Yukki.
 
 """"
 official_radhe_krishna_1 is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Krishna <https://github.com/official_radhe_krishna_1>
+Copyright (c) 2024 -present Team=Krishna <https://github.com/Radheranij/Poison_x_music>
 
 This program is free software: you can redistribute it and can modify
 as you want or you can collabe if you have new ideas.
