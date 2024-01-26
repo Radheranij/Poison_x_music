5acb0e0c76cd61b44a421267c0ca2fca1b098197
Radheranij <157111271+Radheranij@users.noreply.github.com>
2024-01-26 21:35:39 +0530

    Update broadcast.py

--- AlexaMusic/plugins/modules/broadcast.py
+++ AlexaMusic/plugins/modules/broadcast.py
@@ -1,9 +1,9 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
+# Copyright (C) 2024 by Krishna_Help @ Github, < https://github.com/Radharanij >
+# Subscribe On YT < official_radhe_krishna_1 >. All rights reserved. © Krishna © Yukki.
 
 """"
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
+Krishna is a project of Telegram bots with variety of purposes.
+Copyright (c) 2024 -present Team=Krishna <https://github.com/Radheranij>
 
 This program is free software: you can redistribute it and can modify
 as you want or you can collabe if you have new ideas.
@@ -21,9 +21,9 @@ import config
 from config import adminlist, chatstats, clean, userstats
 from pyrogram.enums import ChatMembersFilter
 from strings import get_command
-from AlexaMusic import app, userbot
-from AlexaMusic.misc import SUDOERS
-from AlexaMusic.utils.database import (
+from PoisonMusic import app, userbot
+from PoisonMusic.misc import SUDOERS
+from PoisonMusic.utils.database import (
     get_active_chats,
     get_authuser_names,
     get_client,
@@ -36,8 +36,8 @@ from AlexaMusic.utils.database import (
     update_particular_top,
     update_user_top,
 )
-from AlexaMusic.utils.decorators.language import language
-from AlexaMusic.utils.formatters import alpha_to_int
+from PoisonMusic.utils.decorators.language import language
+from PoisonMusic.utils.formatters import alpha_to_int
 from config import OWNER_ID
 
 BROADCAST_COMMAND = get_command("BROADCAST_COMMAND")
