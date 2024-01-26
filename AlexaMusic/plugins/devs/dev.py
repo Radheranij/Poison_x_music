b2e5ea7c731962ec24f429dcfd7e77a5b5056ceb
Radheranij <157111271+Radheranij@users.noreply.github.com>
2024-01-26 21:12:09 +0530

    Update dev.py

--- AlexaMusic/plugins/devs/dev.py
+++ AlexaMusic/plugins/devs/dev.py
@@ -1,9 +1,9 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
+# Copyright (C) 2024 by Krishna_Help @ Github, < https://github.com/Radharanij >
+# Subscribe On YT < official_radhe_krishna_1 >. All rights reserved. © Poison © Yukki.
 
 """"
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
+Krishna is a project of Telegram bots with variety of purposes.
+Copyright (c) 2024 -present Team=Krishna <https://github.com/Radhranij>
 
 This program is free software: you can redistribute it and can modify
 as you want or you can collabe if you have new ideas.
