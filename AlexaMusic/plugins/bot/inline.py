f534333d0f061074613da115ccbc8a89a7447f87
Radheranij <157111271+Radheranij@users.noreply.github.com>
2024-01-26 21:08:46 +0530

    Update inline.py

--- AlexaMusic/plugins/bot/inline.py
+++ AlexaMusic/plugins/bot/inline.py
@@ -1,9 +1,9 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
+# Copyright (C) 2024 by Kridhna_Help @ Github, < https://github.com/Radheranij >
+# Subscribe On YT < official_radhe_krishna_1 >. All rights reserved. © Poison © Yukki.
 
 """"
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
+Krishna is a project of Telegram bots with variety of purposes.
+Copyright (c) 2024 -present Team=Krishna <https://github.com/Radheranij>
 
 This program is free software: you can redistribute it and can modify
 as you want or you can collabe if you have new ideas.
@@ -18,8 +18,8 @@ from pyrogram.types import (
 from youtubesearchpython.__future__ import VideosSearch
 
 from config import BANNED_USERS, MUSIC_BOT_NAME
-from AlexaMusic import app
-from AlexaMusic.utils.inlinequery import answer
+from PoisonMusic import app
+from PoisonMusic.utils.inlinequery import answer
 
 
 @app.on_inline_query(~BANNED_USERS)
